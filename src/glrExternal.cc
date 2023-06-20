cb8a01cc7e08b688f094e0c59b348baf3fb0166c
Izzy Axel <bencosgriff@gmail.com>
2023-06-20 02:26:37 -0400

    Bug fix

--- src/glrExternal.cc
+++ src/glrExternal.cc
@@ -17,6 +17,7 @@ namespace GLRender
 	std::vector<uint8_t> getPixels(uint32_t width, uint32_t height)
 	{
 		std::vector<uint8_t> out;
+		out.resize(width * height * 3);
 		glReadPixels(0, 0, (GLsizei)width, (GLsizei)height, GL_RGB, GL_UNSIGNED_BYTE, out.data());
 		return out;
 	}
