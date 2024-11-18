176b0da3070d561d99c505c1c251ea14950a0be7
Izzorious Axel <izzyaxel@users.noreply.github.com>
2024-11-18 12:29:13 -0500

    Fix precompile

--- src/precompile.hh
+++ src/precompile.hh
@@ -2,7 +2,10 @@
 
 #include "commons/math/general.hh"
 #include "commons/math/interpolation.hh"
-#include "commons/math/shapes.hh"
+#include "commons/math/linesegment2D.hh"
+#include "commons/math/circle.hh"
+#include "commons/math/aabb2D.hh"
+#include "commons/math/aabb3D.hh"
 #include "commons/math/util.hh"
 #include "commons/math/quaternion.hh"
 #include "commons/math/vec2.hh"
